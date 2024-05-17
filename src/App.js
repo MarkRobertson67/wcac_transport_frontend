1083b29770178bb51312a759974ade38ec869da3
Mark Robertson <markrobertson67@gmail.com>
2024-05-16 20:36:45 -0400

    Added some dependancies and files and folders.  Basic home route working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Home from './components/pages/Home';
+
+// import NavBar from './components/NavBar/NavBar';
+// import Footer from `./components/Footer/Footer`;
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {/* <NavBar /> */}
+        <main>
+          <div className="content-container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+
+            </Routes>
+          </div>
+        </main>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+
+function Home() {
+
+return(
+<div>
+<br></br>
+<br></br>
+<h>Welcome to the home page</h>
+</div>
+)
+}
+
+
+export default Home;
